b998a1a8a79ec269fedba36463122797a62520d0
SaadiaDek <saadiadekkaki62@gmail.com>
2026-02-17 21:50:16 +0000

    Create gallery.js

---
+++ js/gallery.js
@@ -0,0 +1,38 @@
+/*Name this external file gallery.js*/
+
+function upDate(previewPic){
+ /* In this function you should 
+    1) change the url for the background image of the div with the id = "image" 
+    to the source file of the preview image
+    
+    2) Change the text  of the div with the id = "image" 
+    to the alt text of the preview image 
+    */
+  var pic_pr=document.getElementById("image");
+//   pic_pr.style.backgroundImage=`url(${previewPic.src})`;
+ pic_pr.style.backgroundImage="url('"+previewPic.src+"')";
+ pic_pr.innerHTML=previewPic.alt;
+	}
+
+	function unDo(){
+     /* In this function you should 
+    1) Update the url for the background image of the div with the id = "image" 
+    back to the orginal-image.  You can use the css code to see what that original URL was
+    
+    2) Change the text  of the div with the id = "image" 
+    back to the original text.  You can use the html code to see what that original text was
+    */
+   var pic_pr=document.getElementById("image");
+   pic_pr.style.backgroundImage="";
+   pic_pr.style.backgroundColor="#8e68ff";
+   pic_pr.innerHTML="Hover over an image below to display here.";
+		
+	}
+  function addTabFocus(){
+console.log("Good");
+let images = document.querySelectorAll(".preview");
+for(let i=0;i<images.length;i++){
+  images[i].setAttribute("tabindex","0");
+  console.log("Tabindex added "+i);
+}
+  }
